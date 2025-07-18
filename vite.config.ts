2e390d39f9ecb0cfc521361676a19918335b771a
xujie <xujie@titanai.cc>
2025-07-18 10:08:44 +0800

    fix: update Vite configuration to conditionally disable Tailwind CSS for H5 and app platforms

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite';
 import postcssPlugins from './postcss.config';
 
+const isH5 = process.env.UNI_PLATFORM === "h5";
+// vue3 版本构建到 app, UNI_PLATFORM 的值为 app
+// vue2 版本为 app-plus
+const isApp = process.env.UNI_PLATFORM === "app";
+const WeappTailwindcssDisabled = isH5 || isApp;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    uni(),
     UnifiedViteWeappTailwindcssPlugin({
       rem2rpx: true,
-      disabled: false,
+      disabled: WeappTailwindcssDisabled,
     }),
-    uni(),
   ],
   css: {
     postcss: {
